e454a6f5244dd0ebf9a20f86906939aa2f86039d
dmiller23 <dmiller23@unm.edu>
2018-11-13 12:10:37 -0700

    added method shells

--- src/Bank/Bank.java
+++ src/Bank/Bank.java
@@ -4,12 +4,15 @@ package Bank;
  * @author Daniel Miller
  * @version 11-13-18
  */
-public class Bank {
+public class Bank implements Runnable{
+    //list of agent accounts
+    //lists of auction house accounts
+
     /*
     It is static and at a known address
     It hosts
         a list of agent accounts
-        a list of action house accounts
+        a list of auction house accounts
     It shares the list of auction houses with agents having bank accounts
     It provides agents with secret keys for use in the bidding process
     It transfers funds from agent to auction accounts, under agent control
@@ -17,10 +20,44 @@ public class Bank {
 
 
     Will have a proxy
+
+    Some sort of pending balance:
+        every time you make a bid on a new item
+            subtract that amount from pending balance
     */
 
     public static void main(String[] args){
         //todo
     }
+
+    /**
+     * Constructor for Bank
+     *
+     */
+    public Bank(){
+
+    }
+
+
+    @Override
+    public void run() {
+
+    }
+
+
+    /**
+     *
+     * @return
+     */
+    private int assignKey(){
+        return 0;
+    }
+
+    /**
+     * Transfers funds from an Agent account to and AuctionHouse account.
+     */
+//    private void transferFunds(AuctionHouse house, Agent agent, double amount){
+//
+//    }
 
 }
