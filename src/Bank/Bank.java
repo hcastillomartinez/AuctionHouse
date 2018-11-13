c6e3b0eb5a50397546682b892b1697d69c11b7b2
dmiller23 <dmiller23@unm.edu>
2018-11-13 11:54:27 -0700

    added Bank.java

--- src/Bank/Bank.java
+++ src/Bank/Bank.java
@@ -1,6 +1,26 @@
 package Bank;
 
+/**
+ * @author Daniel Miller
+ * @version 11-13-18
+ */
 public class Bank {
+    /*
+    It is static and at a known address
+    It hosts
+        a list of agent accounts
+        a list of action house accounts
+    It shares the list of auction houses with agents having bank accounts
+    It provides agents with secret keys for use in the bidding process
+    It transfers funds from agent to auction accounts, under agent control
+    It blocks and unblocks funds in agent accounts, at the request of action houses
 
+
+    Will have a proxy
+    */
+
+    public static void main(String[] args){
+        //todo
+    }
 
 }
