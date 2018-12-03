cb8891ee7a80c2c0cfa45b011018116a4c2a6fc1
Hector Castillo Martinez <hcastillomartinez@unm.edu>
2018-12-03 12:37:16 -0700

    adding thread to wait for messages from bank, gui updated to display items and auctions.

--- src/AuctionHouse/AuctionHouseGUI.java
+++ src/AuctionHouse/AuctionHouseGUI.java
@@ -1,5 +1,6 @@
 package AuctionHouse;
 
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
@@ -9,6 +10,8 @@ import javafx.stage.Stage;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * GUI for the auction house, testing currently.
@@ -19,16 +22,20 @@ public class AuctionHouseGUI extends Application {
     private static AuctionHouse auctionHouse;
     private List<Item> auctionItems;
     private ListView<Item> displayItems;
+    private List<Auction> auctions;
+    private ListView<Auction> auctionListView;
+    private Timeline timeline;
+    private Timer timer;
     private boolean auctionOpen;
 
 
-    public void setAuctionItems(List<Item> auctionItems) {
-        this.auctionItems = auctionItems;
-    }
-
-    public void setAuctionOpen(boolean auctionOpen) {
-        this.auctionOpen = auctionOpen;
-    }
+//    public void setAuctionItems(List<Item> auctionItems) {
+//        this.auctionItems = auctionItems;
+//    }
+//
+//    public void setAuctionOpen(boolean auctionOpen) {
+//        this.auctionOpen = auctionOpen;
+//    }
 
     /**
      * Used to launch the GUI from another class.
@@ -46,41 +53,58 @@ public class AuctionHouseGUI extends Application {
      * GUI is exited via red x.
      */
     private void updateItemList(){
-        Thread t=new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while(auctionOpen) {
-                    System.out.println("caa");
-                    displayItems.getItems().removeAll();
-                    displayItems.getItems().addAll(auctionItems);
-                }
-            }
-        });
-        t.start();
+        System.out.println("item list");
+        displayItems.getItems().clear();
+        displayItems.getItems().addAll(auctionItems);
+    }
+
+    private void updateAuctionList(){
+        System.out.println("auction list");
+        auctionListView.getItems().clear();
+        auctionListView.getItems().addAll(auctions);
     }
 
     @Override
     public void start(Stage primaryStage) {
+        timeline=new Timeline();
+        timer=new Timer();
         displayItems=new ListView<>();
         auctionItems=auctionHouse.getItemList();
+        auctions=auctionHouse.getAuctions();
+        auctionListView=new ListView<>();
 
-        root.setPrefSize(500,500);
+
+        root.setPrefSize(600,500);
         displayItems.setLayoutX(0);
-        displayItems.setLayoutY(0);
-        displayItems.setPrefSize(350,250);
+        displayItems.setLayoutY(20);
+        displayItems.setPrefSize(300,250);
+        auctionListView.setLayoutY(20);
+        auctionListView.setLayoutX(300);
+        auctionListView.setPrefSize(300,250);
 
-        root.getChildren().add(displayItems);
+        root.getChildren().addAll(displayItems,auctionListView);
         displayItems.getItems().addAll(auctionItems);
-//        updateItemList();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+//                updateItemList();
+//                updateAuctionList();
+            }
+        },1,1);
+
 
         Scene scene=new Scene(root);
+        primaryStage.setResizable(false);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
     @Override
     public void stop(){
+        auctionHouse.sendToBank("close");
         System.out.println("GUI closed");
+        System.exit(1);
+        timer.cancel();
         auctionOpen=false;
     }
 }
